dd8e5eccf468c4da2383a7aa601d95306693cde0
Peter Himmler <pethipet@users.noreply.github.com>
2023-06-02 23:05:01 +0200

    Fixup last commit

---
+++ env_main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+
+    char *VAR_TSK_BEFORE, *VAR_TSK_AFTER = NULL;
+
+    VAR_TSK_BEFORE = getenv("VAR_TSK");
+    _putenv("VAR_TSK=Peter");
+    VAR_TSK_AFTER = getenv("VAR_TSK");
+    printf(" 'Before' %s,  'After' %s\n", VAR_TSK_BEFORE, VAR_TSK_AFTER);
+    _putenv("VAR_TSK=");
+    printf(" 'Before' %s,  'After' %s\n", VAR_TSK_BEFORE, getenv("VAR_TSK"));
+
+    return EXIT_SUCESS;
+     /* EXIT_FAILURE;*/
+}
